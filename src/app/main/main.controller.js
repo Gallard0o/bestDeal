c2d2c95f216a3d22399a03f01185c6516378be95
gallard0o <pop@Lalos-Mac-mini.local>
2016-08-11 15:04:25 -0500

    voy a cambiar el drif por lista

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -8,9 +8,38 @@
   /** @ngInject */
   function MainController($timeout, webDevTec, toastr, $window, $scope, $mdSidenav, $log, httpService) {
     var vm = this;
-
+    vm.isNew=false;
     $scope.toggleRight = buildToggler;
     vm.products = [];
+    vm.filters = [
+        {title: 'mejor coincidencia'},
+        {title: 'mejor calificación'},
+        {title: 'mayor a menor precio'},
+        {title: 'menor a mayor precio'},
+        {title: 'de la A a la Z'},
+        {title: 'de la Z a la A'}
+      ];
+
+      vm.secondfilters = [
+          {title: 'Todo'},
+          {title: 'Nuevo'},
+          {title: 'Usado'}
+        ];
+
+        vm.productlinks = [
+            {logo: 'assets/images/bestbuylogo.png', condition:'usedProduct'},
+            {logo: 'assets/images/ebay.png', condition:'refurbushedProduct'},
+            {logo: 'assets/images/amazon.png', condition:'newProduct'}
+          ];
+
+        vm.checkboxes = [
+            {title: 'NUEVO', class:'checkNew', model:vm.isNew, label:'Nuevo'},
+            {title: 'USADO', class:'checkUsed', model:'isUsed', label:'Usado'},
+            {title: 'REACONDICIONADO', class:'checkRefurbished', model:'isRefurbished', label:'Reacondicionado'}
+          ];
+
+
+      vm.selectedFilter=vm.filter ;
 
     $scope.close = function() {
       // Component lookup should always be available since we are not using `ng-if`
@@ -21,28 +50,29 @@
         });
     };
 
-function searchByCode(query){
-  httpService.searchByCode(vm.selectedProduct.sku).then(function(response){
-    console.log(response.data.products[0])
-    vm.selectedProduct=response.data.products[0];
-  })
-}
+    function searchByCode(query) {
+      httpService.searchByCode(vm.selectedProduct.sku).then(function(response) {
+        console.log(response.data.products[0])
+        vm.selectedProduct = response.data.products[0];
+      })
+    }
 
-    function buildToggler(index) {
-      vm.selectedProductIndex = index;
-          vm.selectedProduct = vm.products[index];
-      searchByCode(vm.selectedProduct.sku);
+    function buildToggler(product) {
+      if (product) {
+        vm.selectedProductIndex = vm.products.indexOf(product);
+        vm.selectedProduct = vm.products[vm.selectedProductIndex];
+        searchByCode(vm.selectedProduct.sku);
 
-      if (!$scope.sidenavOpened) {
-        // Component lookup should always be available since we are not using `ng-if`
-        $mdSidenav('right')
-          .toggle()
-          .then(function() {
-            $scope.sidenavOpened = !$scope.sidenavOpened;
-            $log.debug("toggle " + 'right' + " is done");
-          });
+        if (!$scope.sidenavOpened) {
+          // Component lookup should always be available since we are not using `ng-if`
+          $mdSidenav('right')
+            .toggle()
+            .then(function() {
+              $scope.sidenavOpened = !$scope.sidenavOpened;
+              $log.debug("toggle " + 'right' + " is done");
+            });
+        }
       }
-
     }
 
     vm.buildToggler = buildToggler
@@ -71,12 +101,16 @@ function searchByCode(query){
 
     function search(query) {
       if (query) {
-        httpService.searchByName(query).then(function(data){
-          vm.products=data.data.products;
+        return httpService.searchByName(query).then(function(data) {
+          vm.products = data.data.products;
           console.log(data.data.products)
+          return vm.products;
         });
       }
     }
+
+
+
     /*busqueda ebay*/
 
     //cierres
